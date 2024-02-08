d54c2a0040547bed41d06452521585dcf7295660
mostafa500030 <71846308+mostafa500030@users.noreply.github.com>
2024-02-08 23:22:58 +0200

    DFS link the problem

--- DFS/DFS.cpp
+++ DFS/DFS.cpp
@@ -1,3 +1,5 @@
+// the solutionof problem https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1626
+
 #include <iostream>
 #include <map>
 #include<algorithm>
